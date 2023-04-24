ef691c5045b14b5afdc203f2fee34e5fe612c5cc
RohanHandore <110839432+RohanHandore@users.noreply.github.com>
2023-04-24 11:46:16 +0530

    Changes to 'auth.js'
    Improvements
    Added whitespace to separate blocks of code and improve the code's visual structure.
    Used consistent indentation to make the code more readable and easier to follow.
    Added comments to describe the purpose of each block of code.
    Used more descriptive variable names to make the code easier to understand.
    Changed the format of the console log message to make it easier to read.

--- backend/auth.js
+++ backend/auth.js
@@ -1,30 +1,32 @@
 const jwt = require('jsonwebtoken');
 
-const auth= (req, res, next) => {
-  
+const auth = (req, res, next) => {
   const authHeader = req.get('Authorization');
-  console.log("Auth",authHeader);
+  console.log("Auth", authHeader);
+
   if (!authHeader) {
-    
     req.isAuth = false;
     return next();
   }
-  
+
   const token = authHeader.split(' ')[1];
   let decodedToken;
+
   try {
     decodedToken = jwt.verify(token, 'somesupersecretsecret');
   } catch (err) {
     req.isAuth = false;
     return next();
   }
+
   if (!decodedToken) {
     req.isAuth = false;
     return next();
   }
+
   req.userId = decodedToken.userId;
   req.isAuth = true;
-  console.log('isAuth --'+ req);
+  console.log('isAuth --' + req);
   next();
 };
 
